440ef02622ad3d608fd4c1185211df7afe23531f
Juan Melo Pérez <juanmlstm@gmail.com>
2021-06-28 23:17:25 +0100

    Accordion added

--- src/components/MapDisplay.js
+++ src/components/MapDisplay.js
@@ -1,5 +1,21 @@
 import './MapDisplay.css';
 import React from 'react';
+import Accordion from './Accordion';
+
+const items = [
+    {
+        title: 'Test1',
+        content: 'This is the first test'
+    },
+    {
+        title: 'Test2',
+        content: 'This is the second test'
+    },
+    {
+        title: 'Test3',
+        content: 'This is the third test'
+    }
+];
 
 
 const MapDisplay = props => {
@@ -9,6 +25,7 @@ const MapDisplay = props => {
     return (
         <div >
             <p className="gps-text">Your Latitude is {lat} and your longitude is {long}</p>
+            <Accordion className="accordion" items={items} />
         </div>
     )
 }
